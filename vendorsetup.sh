2dc30cccf7defc6a186404f4574ad5365720ddf5
Eduardo <ealvarado261500@gmail.com>
2024-11-08 17:43:55 -0600

    Enhance vendorsetup.sh; update Fox versioning, add MagiskBoot support, and improve OTA update handling

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -45,15 +45,16 @@ fi
   
 # Maintaining Info
     export OF_MAINTAINER="EduardoA3677"
-    export FOX_VERSION=$(date +%y.%m.%d)
-    export FOX_BUILD_TYPE=Unofficial
-  	
+    export FOX_VERSION="1_EduardoA3677"
+ 
 # Device Info
     export FOX_ARCH=arm64
     export FOX_VARIANT="12.1"
     export TARGET_DEVICE_ALT="PHK110, CPH2487, udon, OP5913L1, OP5961L1, ossi, qssi"
     export FOX_TARGET_DEVICES="PHK110, CPH2487 ,udon ,OP5913L1, OP5961L1, ossi, qssi"
     export FOX_USE_SPECIFIC_MAGISK_ZIP=$(pwd)/device/oneplus/udon/Magisk-v28.0.zip
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 
 # Funtions
     export FOX_REPLACE_BUSYBOX_PS=1
@@ -67,11 +68,12 @@ fi
     export FOX_USE_NANO_EDITOR=1
     export OF_ENABLE_LPTOOLS=1
     export FOX_DELETE_AROMAFM=1
+    export FOX_USE_ZIP_BINARY=1
+
   
 # Display Settings
 
     export OF_ALLOW_DISABLE_NAVBAR=0
-
     export OF_SCREEN_H=2412
     export OF_STATUS_H=106
     export OF_STATUS_INDENT_LEFT=48
@@ -84,14 +86,21 @@ fi
   
 # A/B-Related
     export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
+    export FOX_AB_DEVICE=1
     export FOX_VIRTUAL_AB_DEVICE=1
-    export OF_VANILLA_BUILD=0
   
 # Other Patches
     export OF_FBE_METADATA_MOUNT_IGNORE=1
     export OF_NO_SPLASH_CHANGE=1
     export OF_NO_RELOAD_AFTER_DECRYPTION=1
     export OF_PATCH_AVB20=1
+# OTA for custom ROMs
+    export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+# Run a process after formatting data to work-around MTP issues
+    export OF_RUN_POST_FORMAT_PROCESS=1
 
 	lunch twrp_$FDEVICE-eng
 	# let's see what are our build VARs

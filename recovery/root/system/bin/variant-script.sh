a39e9a7d8fc026c838515c368048cda774d89ebb
Eduardo <ealvarado261500@gmail.com>
2024-11-11 20:55:06 -0600

    Update variant-script.sh; reorder device property settings for India and China variants to improve clarity and maintainability

--- recovery/root/system/bin/variant-script.sh
+++ recovery/root/system/bin/variant-script.sh
@@ -8,21 +8,6 @@ echo $variant
 case $variant in
     "22881")
     #China
-        resetprop ro.product.device "OP5913L1"
-        resetprop ro.product.vendor.device "OP5913L1"
-        resetprop ro.product.odm.device "OP5913L1"
-        resetprop ro.product.product.device "OP5913L1"
-        resetprop ro.product.device "OP5913L1"
-        resetprop ro.product.system_ext.device "OP5913L1"
-        resetprop ro.product.product.model "PHK110"
-        resetprop ro.product.model "PHK110"
-        resetprop ro.product.system.model "PHK110"
-        resetprop ro.product.system_ext.model "PHK110"
-        resetprop ro.product.vendor.model "PHK110"
-        resetprop ro.product.odm.model "PHK110"
-        ;;
-    "22803")
-    #India
         resetprop ro.product.device "OP5961L1"
         resetprop ro.product.odm.device "OP5961L1"
         resetprop ro.product.product.device "OP5961L1"
@@ -35,6 +20,21 @@ case $variant in
         resetprop ro.product.system_ext.model "CPH2487"
         resetprop ro.product.vendor.model "CPH2487"
         resetprop ro.product.odm.model "CPH2487"
+        ;;
+    "22803")
+    #India
+        resetprop ro.product.device "OP5913L1"
+        resetprop ro.product.vendor.device "OP5913L1"
+        resetprop ro.product.odm.device "OP5913L1"
+        resetprop ro.product.product.device "OP5913L1"
+        resetprop ro.product.device "OP5913L1"
+        resetprop ro.product.system_ext.device "OP5913L1"
+        resetprop ro.product.product.model "PHK110"
+        resetprop ro.product.model "PHK110"
+        resetprop ro.product.system.model "PHK110"
+        resetprop ro.product.system_ext.model "PHK110"
+        resetprop ro.product.vendor.model "PHK110"
+        resetprop ro.product.odm.model "PHK110"
         ;;
     *)
         resetprop ro.product.device "OP5961L1"

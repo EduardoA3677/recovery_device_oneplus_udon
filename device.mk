7705d5f4fa7dd0b4a3824db03ef7336ec9215879
Eduardo <ealvarado261500@gmail.com>
2025-01-02 23:22:08 -0600

    Enhance BoardConfig.mk and device.mk for improved recovery and update management

--- device.mk
+++ device.mk
@@ -102,15 +102,13 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
-PRODUCT_PACKAGES += update_engine \
-    update_engine_client \
-    update_verifier \
-
 PRODUCT_PACKAGES += \
-    oplusotapreopt_script
+    update_engine \
+    update_engine_sideload \
+    update_verifier
 
-PRODUCT_PACKAGES += \
-  update_engine_sideload
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 # F2FS Utilities
 PRODUCT_PACKAGES += \
@@ -119,12 +117,9 @@ PRODUCT_PACKAGES += \
     check_f2fs
 
 # Userdata checkpoint
-PRODUCT_PACKAGES += \
-    checkpoint_gc
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/oplusotapreopt_script \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
@@ -133,6 +128,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
 
 # Stock OEM OTA Cert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
